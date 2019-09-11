985703803ef0c9a3544ab87044eced8a5b9b54ff
EdithChorev <EdithChorev@gmail.com>
2019-09-11 14:52:28 +0200

    cleaned model file

--- code/model.py
+++ code/model.py
@@ -6,7 +6,9 @@ import keras
 from keras.models import Sequential,Model
 from keras.layers import  Conv2D, Flatten, Dense, MaxPool2D, Dropout
 from keras.layers import Concatenate, concatenate
-
+from keras.callbacks import History
+from sklearn.metrics import f1_score, cohen_kappa_score, accuracy_score
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
@@ -41,8 +43,8 @@ def get_data(batchsize =16): ## cnn only part
     
     return np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test) 
     
-def cnn_model1(name,rate):
-    x_shaped=(3,3000,1)
+def cnn_model1(name,rate,x_shaped):
+    
     my_cnn = Sequential(name=name)
     my_cnn.add(Conv2D(filters=64, kernel_size=(50,1), strides=(8,1), padding="same", input_shape=x_shaped, activation="relu"))
     my_cnn.add(MaxPool2D(pool_size=(8,1), strides=(8,1), padding="same"))
@@ -54,8 +56,8 @@ def cnn_model1(name,rate):
     
 
     return my_cnn
-def cnn_model2(name,rate):
-    x_shaped=(3,3000,1)
+def cnn_model2(name,rate,x_shaped):
+    
     my_cnn = Sequential(name=name)
     my_cnn.add(Conv2D(filters=64, kernel_size=(400,1), strides=(50,1), padding="same", input_shape=x_shaped, activation="relu"))
     my_cnn.add(MaxPool2D(pool_size=(4,1), strides=(4,1), padding="same"))
@@ -66,9 +68,11 @@ def cnn_model2(name,rate):
     my_cnn.add(Flatten())
     
     return my_cnn
-def build_merged_model(keep_proba):
-    model1 = cnn_model1('model1',keep_proba)
-    model2 = cnn_model2('modle2',keep_proba)    
+
+def build_merged_model(keep_proba,x_shaped):
+    x_shaped=(3,3000,1)
+    model1 = cnn_model1('model1', keep_proba, x_shaped)
+    model2 = cnn_model2('modle2', keep_proba, x_shaped)    
     x_shaped=(3,3000,1)
     features = keras.layers.Input(x_shaped)
     features_shape = tf.reshape(features,(-1,3,3000,1))
@@ -79,102 +83,32 @@ def build_merged_model(keep_proba):
 
     return model
 
-# def cnn_part (inputs_var, rate=0.5):
-    
-#     output_conns=[]
-#      ### short filter cnn
-#     my_cnn = tf.layers.conv2d(inputs=input_var, filters=64, kernel_size=(50,1),
-#                                 strides=(6,1), padding='same', activation=tf.nn.relu) 
-#     my_cnn = tf.layers.max_pooling2d(inputs=my_cnn, pool_size=(8,1), strides=(8,1), padding='same')
-#     my_cnn = tf.nn.dropout(my_cnn, rate)
-#     for _ in range(3):
-#         my_cnn = tf.layers.conv1d(inputs=my_cnn, filters=128, kernel_size=(8,1), strides=(1,1),
-#                                  padding='same', activation=tf.nn.relu)
-#     my_cnn = tf.layers.max_pooling1d(inputs=my_cnn, pool_size=(4,1), strides=(4,1), padding='same')
-#     my_cnn = flatten(name="flat1", input_var=my_cnn)
 
-#     output_conns.append(my_cnn)
-#     ### long filter cnn
-#     my_cnn = tf.layers.conv2d(inputs=input_var, filters=64, kernel_size=(400,1),
-#                                 strides=(50,1), padding='same', activation=tf.nn.relu) 
-#     my_cnn = tf.layers.max_pooling2d(inputs=my_cnn, pool_size=(4,1), strides=(4,1), padding='same')
-#     my_cnn = tf.nn.dropout(my_cnn, rate)
-#     for _ in range(3):
-#         my_cnn = tf.layers.conv1d(inputs=my_cnn, filters=128, kernel_size=(6,1), strides=(1,1),
-#                                  padding='same', activation=tf.nn.relu)
-#     my_cnn = tf.layers.max_pooling1d(inputs=my_cnn, pool_size=(2,1), strides=(2,1), padding='same')
-#     my_cnn = flatten(name="flat2", input_var=my_cnn)
 
-#     output_conns.append(my_cnn)
-    
-#     #concat flattened layers
-#     my_cnn = tf.concat (output_conns, 1, name='concat1' )
-#     my_cnn = tf.nn.dropout(my_cnn,rate)
-#     #dense layer
-#     my_cnn = tf.layers.dense(my_cnn, filters=128, activation=tf.nn.relu)
-#     my_cnn = tf.layers.dense(my_cnn, filters=5)
-#     return my_cnn
-def cnn_part(keep_proba=0.5):
-    
-    ### short filter cnn
-    x_shaped = (3,3000,1)
-    
-    features =keras.layers.Input(x_shaped)
-    features_shape = tf.reshape(features,(-1,3,3000,1))
-    my_cnn = Conv2D ( filters=64, kernel_size=(50,1), 
-                    strides=(6,1), padding='same', activation ="relu")(features_shape)
-    my_cnn = MaxPool2D ( pool_size = (8,1), strides = (8,1), padding='same')(my_cnn)
-    my_cnn = Dropout( rate=1-keep_proba)(my_cnn)
-    for _ in range (3): 
-        my_cnn = Conv2D (filters=128, kernel_size=(8,1), 
-                    strides=(1,1), padding='same', activation ="relu")(my_cnn)
-    
-    my_cnn = MaxPool2D ( pool_size = (4,1), strides = (4,1), padding='same')(my_cnn)
-    my_cnn= Flatten()(my_cnn)
-    #output_conns.append(my_cnn)
-    ### long filter cnn
-    my_cnn2 = Conv2D ( filters=64, kernel_size=(400,1), 
-                    strides=(50,1), padding='same', activation ="relu")(features_shape)
-    my_cnn2 = MaxPool2D ( pool_size = (4,1), strides = (4,1), padding='same')(my_cnn2)
-    my_cnn2 = Dropout(rate=keep_proba)(my_cnn2) 
-    for _ in range (3): 
-        my_cnn2 = Conv2D (filters=128, kernel_size=(6,1), 
-                    strides=(1,1), padding='same', activation ="relu")(my_cnn2)
-    
-    my_cnn2 = MaxPool2D ( pool_size = (2,1), strides = (2,1), padding='same')(my_cnn2)
-    my_cnn2 = Flatten()(my_cnn2)
-    #output_conns.append(my_cnn)
-    ### concat output of cnns
-    # my_cnn = tf.concat (output_conns, 1, name='concat1' )
-    my_cnn = Concatenate()([my_cnn,my_cnn2])
-    
-    
-    ### add fully connected layer
-    my_cnn = Dense (units=128, activation = "relu")(my_cnn)
-    #my_cnn = Dropout (rate=keep_proba)(my_cnn)
-    classes = Dense ( units=5, activation = "softmax")(my_cnn)
-    
-    model=Model(inputs= [my_cnn.input,my_cnn2.input], outputs=classes)
-    
-    return model
 
 def run_all():
-    # tf.enable_eager_execution()
-    X_train, X_test, y_train, y_test  = get_data()
-    # x_shaped = tf.placeholder(tf.float32,[3,3000])
-    # output = tf.placeholder(tf.float32,[None,5])
+    history = History()
+    acc_tr = []
+    loss_tr = []
     
-    #model = cnn_model ('my_cnn',0.5)
+
+    X_train, X_test, y_train, y_test  = get_data()
+   
     
     batch_size = 16
-    # model=cnn_part(keep_proba = 0.5)
+   
     model=build_merged_model(keep_proba=0.5)
     optimizer = keras.optimizers.Adam(lr=0.0001,clipnorm=1.0)
     train_loss = keras.losses.categorical_crossentropy
    
-    model.compile(optimizer=optimizer, loss=train_loss)
+    model.compile(optimizer=optimizer, loss=train_loss, metrics=['accuracy'])
     print(model.summary())
-    model.fit([X_train.reshape(-1,3,3000,1),X_train.reshape(-1,3,3000,1)],y_train,batch_size=batch_size, epochs=10,verbose=2)#,
+    model.fit([X_train.reshape(-1,3,3000,1),X_train.reshape(-1,3,3000,1)],
+                y_train, batch_size=batch_size, epochs=10, verbose=1,
+                callbacks=[history])#,
+               
+    acc_tr.append(history.history['acc'])
+	loss_tr.append(history.history['loss'])
                 # callbacks=keras.callbacks.EarlyStopping(monitor='loss',restore_best_weights=True) )
     
     ## add train data to eval
