1a86650797c4a5914f3899439680b176ebd37fa9
EdithChorev <EdithChorev@gmail.com>
2019-09-13 16:25:36 +0200

    model2 with custom loss function

--- code/model2.py
+++ code/model2.py
@@ -9,6 +9,7 @@ from keras.layers import Concatenate, concatenate,BatchNormalization
 from keras.layers import  GRU, Input, LSTM, TimeDistributed, Bidirectional
 from keras.callbacks import History
 import keras.backend as K
+from keras.models import load_model
 from sklearn.metrics import f1_score, cohen_kappa_score, accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
@@ -18,7 +19,7 @@ import json
 import pickle
 
 
-def get_data(fold): ## cnn only part
+def get_data(fold,seq_length): ## cnn only part
     name='/home/edith/Documents/EEG/2018/folds/fold'+str(fold)+'_X.npy'
     x = np.load(name)
     name='/home/edith/Documents/EEG/2018/folds/fold'+str(fold)+'_Y.npy'
@@ -43,14 +44,13 @@ def get_data(fold): ## cnn only part
 
     y=np.array(y)
     y=OneHotEncoder(sparse=False).fit_transform(y.reshape(-1,1))
-    x=np.split(np.array(x), len(x)//5, axis=0)
-    y=np.split(np.array(y), len(y)//5, axis=0)
+    x=np.split(np.array(x), len(x)//seq_length, axis=0)
+    y=np.split(np.array(y), len(y)//seq_length, axis=0)
     # per_cat=np.sum(y,axis=0)
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, 
-                random_state=42)
+    
    
     
-    return np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test) 
+    return np.array(x), np.array(y) 
     
 def cnn_model1(name,rate,x_shaped):
     input = Input(shape=x_shaped)
@@ -79,8 +79,8 @@ def cnn_model2(name,rate,x_shaped):
 
 
 def build_merged_model(keep_proba,x_shaped,seq_len):
-    input1 = Input(shape=(None,3,3000,1))
-    input2 = Input(shape=(None,3,3000,1))
+    input1 = Input(shape=(None,x_shaped[0],x_shaped[1],x_shaped[2]))
+    input2 = Input(shape=(None,x_shaped[0],x_shaped[1],x_shaped[2]))
     model1 = cnn_model1('model1', keep_proba, x_shaped)
     model2 = cnn_model2('modle2', keep_proba, x_shaped)   
     model1_t = TimeDistributed(model1)(input1)
@@ -98,17 +98,30 @@ def build_merged_model(keep_proba,x_shaped,seq_len):
 
 
 
-def w_categorical_crossentropy():
-    def loss(y_true, y_pred):
-        weights=np.array([0.18608124, 0.13560335, 0.43690263, 0.08632019, 0.15509259])
-        nb_cl = len(weights)
-        final_mask = K.zeros_like(y_pred[:, 0])
-        y_pred_max = K.max(y_pred, axis=1, keepdims=True)
-        y_pred_max_mat = K.equal(y_pred, y_pred_max)
-        for c_p, c_t in product(range(nb_cl), range(nb_cl)):
-            final_mask += (weights[[c_t,c_p]] * y_pred_max_mat[:, c_p] * y_true[:, c_t])
-        return K.categorical_crossentropy(y_pred, y_true) * final_mask
+def w_categorical_crossentropy(y_true, y_pred):
+    """
+    A weighted version of keras.objectives.categorical_crossentropy
+    
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+    
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    """
+    weights=np.array([0.18608124, 0.13560335, 0.43690263, 0.08632019, 0.15509259])
+    weights = K.variable(weights)
+    # scale predictions so that the class probas of each sample sum to 1
+    y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+    # clip to prevent NaN's and Inf's
+    y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+    # calc
+    loss = y_true * K.log(y_pred) * weights
+    loss = -K.sum(loss, -1)
     return loss
+
+    
 
 def run_all():
     history = History()
@@ -116,28 +129,36 @@ def run_all():
     loss_tr = []
     cm=[]
     ck_score=[]
-    batch_size =2
-    x_shaped=(3,3000,1)
+    batch_size = 4
+    num_epochs = 10
+    num_channels = 3
+    epi_samples =3000
+    seq_len = 10
+    x_shaped=(num_channels,epi_samples,1)
     model=build_merged_model(keep_proba=0.5, x_shaped=x_shaped,seq_len=10)
     optimizer = keras.optimizers.Adam(lr=0.0001,clipnorm=1.0)
-    train_loss = keras.losses.categorical_crossentropy
+    # train_loss = keras.losses.categorical_crossentropy
    
-    model.compile(optimizer=optimizer, loss=train_loss, metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss=w_categorical_crossentropy, metrics=['accuracy'])
     print(model.summary())
+    val_fold = np.random.permutation(10)
     for fold in range(1):
-        X_train, X_test, y_train, y_test  = get_data(fold)
-        history=model.fit([X_train.reshape(-1,5,3,3000,1),X_train.reshape(-1,5,3,3000,1)],
-                y_train, batch_size=batch_size, epochs=5, verbose=1, 
-                callbacks=[history])#,validation_data=([X_test.reshape(-1,3,3000,1),X_test.reshape(-1,3,3000,1)],y_test),
-          
-        history_dict=history.history 
-        json.dump(history_dict, open('/home/edith/Documents/EEG/history.json', 'w'))   
-        acc_tr.append(history_dict['acc'])
-        loss_tr.append(history_dict['loss'])
-        y_hat=model.predict([X_test.reshape(-1,3,3000,1),X_test.reshape(-1,3,3000,1)], batch_size=batch_size, verbose=1)
-        cm.append(confusion_matrix(y_test, y_hat, labels=range(5)))
-        ck_score.append(cohen_kappa_score(y_test, y_hat))
-                # callbacks=keras.callbacks.EarlyStopping(monitor='loss',restore_best_weights=True) )
+        if fold != val_fold[fold]:
+            X_train,  y_train  = get_data(fold,seq_len)
+            history=model.fit([X_train.reshape(-1,seq_len,num_channels,epi_samples,1),X_train.reshape(-1,seq_len,num_channels,epi_samples,1)],
+                    y_train, batch_size=batch_size, epochs=num_epochs, verbose=1, 
+                    callbacks=[history])#,validation_data=([X_test.reshape(-1,3,3000,1),X_test.reshape(-1,3,3000,1)],y_test),
+            
+            history_dict=history.history 
+            json.dump(history_dict, open('/home/edith/Documents/EEG/history.json', 'w'))   
+            acc_tr.append(history_dict['acc'])
+            loss_tr.append(history_dict['loss'])
+            model.save('model2.h5')
+            X_test,  y_test  = get_data(val_fold[fold],seq_len)
+            y_hat=model.predict([X_test.reshape(-1,num_channels,epi_samples,1),X_test.reshape(-1,num_channels,epi_samples,1)], batch_size=batch_size, verbose=1)
+            cm.append(confusion_matrix(y_test, y_hat, labels=range(5)))
+            ck_score.append(cohen_kappa_score(y_test, y_hat))
+                    # callbacks=keras.callbacks.EarlyStopping(monitor='loss',restore_best_weights=True) )
     
     ## add train data to eval
     # balance data
